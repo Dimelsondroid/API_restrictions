5bdffb3fbc9ccef579a029bde168cf82af8a0f49
Dimelsondr <dimuxagor@gmail.com>
2022-10-29 19:21:12 +0300

    Initial

---
+++ advertisements/views.py
@@ -0,0 +1,23 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwner
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+    filterset_fields = ['creator', 'created_at', 'updated_at', 'status']
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create"]:
+            return [IsAuthenticated()]
+        if self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwner()]
+        return []
